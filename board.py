4cd93b8d1d3ee1cec46be42d83c780eb0784688b
JGalvez1776 <64766167+JGalvez1776@users.noreply.github.com>
2020-05-20 22:58:20 -0700

    Update board.py

--- board.py
+++ board.py
@@ -1,12 +1,9 @@
 """ Author: Justin Galvez
     Name: board.py
-    Description:
+    Description: 
 """
 
 from piece import *
-
-
-
 
 
 class Board:
@@ -16,35 +13,45 @@ class Board:
         self._hei = hei
         for i in range(wid):
             self._board[i] = [None] * wid
-
-    def place(self, piece, x, y):
-        if piece is not None:
-            piece.update_position(x, y)
-        self._board[row(y)][x] = piece
+        self._kings_locations = None
 
     def print(self):
-        length = len(self.get_board()) * 6 + 1
-        print('-' * length)
-        for row in self.get_board():
-            print('|', end='')
+        board = self.get_board()
+        height = self.get_hei()
+        wid = self.get_wid()
+        length = len(board) * 6 + 1
+        letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[0:wid]
+        print()
+        print('  ' + '-' * length)
+        for row in board:
+            print(f'{height} |', end='')
             for square in row:
                 if square is None:
                     print('     ', end='|')
                 else:
                     print(str(square), end='|')
             print()
-            print('-' * length)
+            print('  ' + '-' * length)
+            height -= 1
+        print('   ', end= '')
+        for letter in letters:
+            print(f'  {letter}   ', end= '')
+        print()
+
+    def place(self, piece, x, y):
+        if piece is not None:
+            piece.update_position(x, y)
+        self._board[row(y)][x] = piece
 
     def move(self, x1, y1, x2, y2):
         '''
         This assumes that a move is valid
         '''
         piece = self.get_square(x1, y1)
+        piece.update_move_status()
         self.place(piece, x2, y2)
         self.place(None, x1, y1)
-        #piece.find_moves(self, x2, y2)
-        self.find_all_moves()
-
+        #self.find_all_moves()
 
     def get_square(self, x, y):
         return self._board[row(y)][x]
@@ -63,7 +70,7 @@ class Board:
         old_y = y
         y = row(y)
         for i in range(len(board[y])):
-            pawn = generate_piece(team, piece)
+            pawn = generate_piece(team, piece, 'Pawn')
             board[y][i] = pawn
             pawn.update_position(i, old_y)
 
@@ -76,8 +83,25 @@ class Board:
                 pos = square.get_position()
                 square.find_moves(self, pos[0], pos[1])
 
+    def update_kings_locations(self):
+        board = self.get_board()
+        locations = []
+        for y in range(0, self.get_hei()):
+            for x in range(0, self.get_wid()):
+                if board[y][x] is not None and board[y][x].get_type() == 'King':
+                    locations.append((x, y))
+        self.set_kings_locations(locations)
+
+
+    def set_kings_locations(self, val):
+        self._kings_locations = val
+
+    def get_kings_locations(self):
+        return self._kings_locations
+
 
 def row(y):
+    # Exists since my code is jank
     return -(y + 1)
 
 
@@ -86,22 +110,23 @@ def create_standard_board():
     moves = generate_standard_moves()
     board.fill_row('B', 'Pawn', 6)
     board.place(generate_piece('B', 'Rook', moves), 0, 7)
-    board.place(generate_piece('B', 'Rook', moves), 7, 7)
     board.place(generate_piece('B', 'Knight', moves), 1, 7)
-    board.place(generate_piece('B', 'Knight', moves), 6, 7)
     board.place(generate_piece('B', 'Bishop', moves), 2, 7)
-    board.place(generate_piece('B', 'Bishop', moves), 5, 7)
     board.place(generate_piece('B', 'Queen', moves), 3, 7)
     board.place(generate_piece('B', 'King', moves), 4, 7)
+    board.place(generate_piece('B', 'Bishop', moves), 5, 7)
+    board.place(generate_piece('B', 'Knight', moves), 6, 7)
+    board.place(generate_piece('B', 'Rook', moves), 7, 7)
 
     board.fill_row('W', 'Pawn', 1)
     board.place(generate_piece('W', 'Rook', moves), 0, 0)
-    board.place(generate_piece('W', 'Rook', moves), 7, 0)
     board.place(generate_piece('W', 'Knight', moves), 1, 0)
-    board.place(generate_piece('W', 'Knight', moves), 6, 0)
     board.place(generate_piece('W', 'Bishop', moves), 2, 0)
-    board.place(generate_piece('W', 'Bishop', moves), 5, 0)
     board.place(generate_piece('W', 'Queen', moves), 3, 0)
     board.place(generate_piece('W', 'King', moves), 4, 0)
+    board.place(generate_piece('W', 'Bishop', moves), 5, 0)
+    board.place(generate_piece('W', 'Knight', moves), 6, 0)
+    board.place(generate_piece('W', 'Rook', moves), 7, 0)
+
     board.find_all_moves()
     return board
